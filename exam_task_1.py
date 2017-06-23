d6f54e58e7027cde9b4f328c0bdc7ff3242b068b
NALM98 <n.myagkov@mail.ru>
2017-06-23 14:20:43 +0400

    Delete exam_task_1.py

--- exam_task_1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for root, dirs, files in os.walk('.\\news\\'):
-    for f in files:
-        with open (os.path.join(root, f), 'r', encoding = 'cp1251') as text:
-            words = text.read()
-            print(f, '\t', words.count('ana'))
-                
-            
